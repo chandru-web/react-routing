7483d07112fba63f7ea37b43f7d3eccc140507aa
chandru-web <chandrusekar.github@gmail.com>
2024-09-25 10:08:37 +0000

    Initial commit to react routing

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const Home = () => (
+  <>
+    <div className="Home-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+        alt="home"
+        className="home-image"
+      />
+      <br />
+      <h1 className="home-text">Home</h1>
+    </div>
+  </>
+)
+export default Home
